15487cc4ad33e8af5bfd4a14de834244efcde12f
Thiago Paiva <thiagohdaqw@gmail.com>
2026-03-28 22:02:46 -0300

    Added basic stun header parser

---
+++ src/stun.h
@@ -0,0 +1,59 @@
+#ifndef __STUN_H_INCLUDED__
+#define __STUN_H_INCLUDED__
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    BindingRequest  = 0x0001,
+    BindingResponse = 0x0101,
+} StunMessageType;
+
+typedef struct {
+    unsigned short type;
+    unsigned short message_len;
+    unsigned long transaction_1;
+    unsigned long transaction_2;
+} StunMessageHeader;
+
+
+typedef struct {
+    StunMessageHeader header;
+} StunMessage;
+
+bool stun_parse_message(StunMessage *message, const char *data,
+                        size_t data_len);
+
+#endif
+
+#ifndef __STUND_H_IMP__
+#define __STUND_H_IMP__
+
+#define consume(out, data, data_len)                                           \
+    do {                                                                       \
+        if (data_len < sizeof(out))                                            \
+            return false;                                                      \
+        memcpy(&(out), (data), sizeof(out));                                   \
+        data += sizeof(out);                                                   \
+        data_len -= sizeof(out);                                               \
+    } while (0)
+
+// https://datatracker.ietf.org/doc/html/rfc5389#section-6
+bool stun_parse_message(StunMessage *message, const char *data, size_t data_len) {
+    if (!data || data_len <= 0) return false;
+    consume(message->header.type, data, data_len);
+    consume(message->header.message_len, data, data_len);
+    
+    consume(message->header.transaction_1, data, data_len);
+    consume(message->header.transaction_2, data, data_len);
+
+    unsigned int magic = 0;
+    memcpy(&magic, &message->header.transaction_1, sizeof(magic));
+    if (magic != 0x2112A442) {
+        return false;
+    }
+    return true;
+}
+
+#endif
--- src/stun_server.c
+++ src/stun_server.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 #include"udp_server.h"
+#include"stun.h"
 
 #define PORT 3478
 
 void on_message_received(void* this, Client client) {
     printf("Client message: %s", client.message);
+
+    StunMessage message = {0};
+    
+    if (!stun_parse_message(&message, client.message, client.message_len)) {
+        printf("Failed to parse client messsage\n");
+        return;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -12,6 +20,5 @@ int main(int argc, char *argv[]) {
     UDPServer server = udp_server_create(PORT);
     server.on_message_received = &on_message_received;
     
-    udp_server_listen(&server);
-    return 0;
+    return !udp_server_listen(&server);
 }
